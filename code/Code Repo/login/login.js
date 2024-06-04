5a704bd6c652bb4117b4bfd0d7c6dd9b3054d20c
Midhun P M <midhun122015@gmail.com>
2024-06-04 17:03:42 +0530

    front end updated

---
+++ code/Code Repo/login/login.js
@@ -0,0 +1,12 @@
+function logstd() {
+   window.location.href = "RepoStudentLogin.html" 
+}
+function logt() {
+   window.location.href = "RepoTeacherlogin.html" 
+}
+function signt() {
+   window.location.href = "repoTeacherSignup.html" 
+}
+function signstd() {
+   window.location.href = "repoStudentSignup.html" 
+}
